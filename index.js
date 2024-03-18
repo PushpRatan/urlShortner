9486f1ce78f5d746dd63c9b73c97bac49fecdaf2
Pushp RataN <Pushpratan1@gmail.com>
2024-03-18 09:43:09 +0530

    trying to deploy

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const path = require("path");
 const staticRouter = require("./routes/staticRouter");
 
 const app = express();
-const PORT = 4444;
+const PORT = process.env.PORT || 4444;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-connectToMongoDB("mongodb://127.0.0.1:27017/urlShortner").then(() => {
+connectToMongoDB(process.env.MONGODB_URI).then(() => {
   console.log("MongoDb Connected.");
 });
 
